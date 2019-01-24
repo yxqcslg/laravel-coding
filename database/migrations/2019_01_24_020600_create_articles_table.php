b5f251f0ddd06a75591ce1a245e5132d58cb0af4
yuxiaoqing <yuxiaoqing@jingdigital.com>
2019-01-24 10:17:56 +0800

    init

---
+++ database/migrations/2019_01_24_020600_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 100)->comment();
+            $table->string('slug',100)->unique()->comment();
+            $table->text('content')->comment();
+            $table->timestamp('published_at')->nullable()->comment();
+            $table->integer()
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
