e96df5cf607dec702363c68491995f5464b8eb9a
yuxiaoqing <yuxiaoqing@jingdigital.com>
2019-01-24 10:30:14 +0800

    init

--- database/migrations/2019_01_24_020600_create_articles_table.php
+++ database/migrations/2019_01_24_020600_create_articles_table.php
@@ -15,11 +15,13 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title', 100)->comment();
-            $table->string('slug',100)->unique()->comment();
-            $table->text('content')->comment();
-            $table->timestamp('published_at')->nullable()->comment();
-            $table->integer()
+            $table->string('title', 100)->comment('标题');
+            $table->string('slug',100)->unique()->comment('别名');
+            $table->text('content')->comment('内容');
+            $table->timestamp('published_at')->nullable()->comment('发布时间');
+            $table->integer('user_id')->comment('用户 ID');
+            $table->integer('category_id')->comment('分类 ID');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
